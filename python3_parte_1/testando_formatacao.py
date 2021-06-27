dd8d379fe9526213d5b3fc2ab7b814d7ed769a7d
Michelle Lira <mchll.lira@gmail.com>
2021-06-27 16:14:40 -0300

    Adiciona pasta do curso de introdução ao Python - parte 1

---
+++ python3_parte_1/testando_formatacao.py
@@ -0,0 +1,15 @@
+dia_ini = 24
+dia_fim = 28
+mes = "fevereiro"
+ano = 2017
+
+print("Em {} o carnaval acontece em {} do dia {} ao dia {}".format(ano, mes, dia_ini, dia_fim))
+
+
+nome = "Maria"
+sobrenome = "Oliveira"
+print(nome, sobrenome, sep=" ")
+
+
+idade_str = input("Digite sua idade: ")
+idade = int(idade_str)
